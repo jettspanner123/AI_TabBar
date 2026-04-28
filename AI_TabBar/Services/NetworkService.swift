b2c724966173db7f7bc2321494df4f1d27e8d5f0
Uddeshya Singh <uddeshyasingh12bsci@gmail.com>
2026-04-28 14:12:23 +0530

    Migration: Fast api python to gin golang

--- AI_TabBar/Services/NetworkService.swift
+++ AI_TabBar/Services/NetworkService.swift
@@ -14,10 +14,22 @@ class NetworkGetServiceHelper {
 
 class NetworkPostServiceHelper {
     public func getMultipleChoiseQuestionAnswer(imageData: Data?) async throws -> APIResponse<MCQDataResponse> {
-        guard let imageData else { return APIResponse(success: false, message: "Image Data Not Found!", data: nil)}
+        guard let imageData else {
+            return APIResponse(success: false, message: "Image Data Not Found!", data: nil)
+        }
         
-        let requestURL = URLRequest(url: NetworkServiceHelper.current.constructURL(endpoint: .ai, subEndpoint: .getMCPAnswer))
+        var requestURL = URLRequest(url: NetworkServiceHelper.current.constructURL(endpoint: .ai, subEndpoint: .getMCPAnswer)!)
         requestURL.httpMethod = "POST"
+        requestURL.httpBody = imageData
         
+        let (data, _) = try await URLSession.shared.data(for: requestURL)
+        
+        do {
+            return try JSONDecoder().decode(APIResponse<MCQDataResponse>.self, from: data)
+        } catch {
+            print("Decoding error:", error)
+            print(String(data: data, encoding: .utf8) ?? "Invalid response")
+            return APIResponse(success: false, message: "Decoding Error! \(error)", data: nil)
+        }
     }
 }
